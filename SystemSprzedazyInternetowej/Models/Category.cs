615ac68fe5ec7ee06d125bb9397fc2df7348c971
Albert Wagner <bertson500@kul.lublin.pl>
2019-02-26 11:27:41 +0100

    Dodanie klasy Cloth

---
+++ SystemSprzedazyInternetowej/Models/Category.cs
@@ -0,0 +1,6 @@
+﻿namespace SystemSprzedazyInternetowej.Models
+{
+    public class Category
+    {
+    }
+}
---
+++ SystemSprzedazyInternetowej/Models/Cloth.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SystemSprzedazyInternetowej.Models
+{
+    public class Cloth
+    {
+        public int ClothId { get; set; }
+        public int CategoryId { get; set; }
+        public string NameOfClothes { get; set; }
+        public string SizeCloth { get; set; }
+        public string ColorCloth { get; set; }
+        public DateTime DateAddCloth { get; set; }
+        public string ImageClothFileName { get; set; }
+        public string DescriptionCloth { get; set; }
+        public decimal ClothPrice { get; set; }
+        public bool SaleCloth { get; set; }
+        public int SecretCloth { get; set; }
+        public string ClotCutDescription { get; set; }
+
+        public virtual Category Category { get; set; }
+
+    }
+}
