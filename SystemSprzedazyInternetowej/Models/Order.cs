2fa02ec407d7145d773759452da7e58ac2b97999
Albert Wagner <bertson500@kul.lublin.pl>
2019-02-26 11:52:41 +0100

    dodanie klas

---
+++ SystemSprzedazyInternetowej/Models/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SystemSprzedazyInternetowej.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AdressEmail { get; set; }
+        public string Comment { get; set; }
+        public DateTime OrderDateAdd { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public decimal ValueOrder { get; set; }
+
+        public List<OrderPossition> OrderPossition { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        New,
+        Realized
+    }
+}
---
+++ SystemSprzedazyInternetowej/Models/OrderPossition.cs
@@ -0,0 +1,17 @@
+﻿namespace SystemSprzedazyInternetowej.Models
+{
+    public class OrderPossition
+    {
+        public int OrderPossitionId { get; set; }
+        public int OrderId { get; set; }
+        public int ClothId { get; set; }
+        public int Quantity { get; set; }
+        public decimal PriceOrderFinal { get; set; }
+
+        public virtual Cloth Cloth { get; set; }
+        public virtual Order Order { get; set; }
+
+
+
+    }
+}
